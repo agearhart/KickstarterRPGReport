4dc6e3f402c48ec3f9f15837476969741c196918
Andy Gearhart <andy.gear@gmail.com>
2020-03-08 16:30:16 +0000

    use XPath query to fetch all the projects from each Kickstarter page request

--- KickstarterRpgReport.py
+++ KickstarterRpgReport.py
@@ -1,17 +1,19 @@
 import requests
 import logging
 
-from kickstarterproject import KickstarterProject
+from lxml import html
 from typing import List
 
 kickstarter_rpg_query = "https://www.kickstarter.com/discover/advanced?state=live&term=rpg&category_id=34&sort=newest&page="
-pages: List[str] = []
-parsed_projects: List[KickstarterProject] = []
+parsed_projects: List[str] = []
 
 
 def get_next_page(last_page: int) -> str:
-    """Give the last page number fetched; fetch the next page"""
-    url: str = kickstarter_rpg_query + str(last_page + 1)
+    """Given the last page number fetched; fetch the next page"""
+    current_page: int = last_page + 1
+    logging.info("Fetching Page {}".format(current_page))
+
+    url: str = kickstarter_rpg_query + str(current_page)
     response = requests.get(url)
     if response.ok:
         return response.text
@@ -20,24 +22,31 @@ def get_next_page(last_page: int) -> str:
 
 
 def get_projects_from_page(page: str) -> List[str]:
-    return []
+    """Scrape the webpage passed in for projects
+
+    Using methods learned from https://docs.python-guide.org/scenarios/scrape/
+    """
+    xml_tree = html.fromstring(page)
+    project_divs = xml_tree.xpath('//div/@data-project')  # get all the div tags containing data-project attributes
+
+    return project_divs
 
 
 def main():
     page_count: int = 0
-    continue_fetching: bool = True
+    last_page_project_count:int = 1 # a white lie to allow processing of the first loop
 
-    while continue_fetching:
+    while last_page_project_count > 0:
         page: str = get_next_page(page_count)
         projects = get_projects_from_page(page)
-        if len(projects) is not 0:
-            continue_fetching = False
-        else:
-            ++page_count
-            for project in projects:
-                parsed_projects.append(KickstarterProject(project))
+        last_page_project_count = len(projects)
 
-    logging.info("Processed {} pages".format(page_count))
+        if last_page_project_count is not 0:
+            page_count += 1
+            for project in projects:
+                parsed_projects.append(project)
+
+    logging.info("Processed {} pages for {} projects".format(page_count, len(parsed_projects)))
 
 
 if __name__ == "__main__":
--- kickstarterproject.py
+++
@@ -1,9 +0,0 @@
-class KickstarterProject:
-    """An individual Kickstarter Project parsed out from a KickstarterPage"""
-
-    def __init__(self, project_text: str):
-        """Constructor
-
-        :param project_text: the raw div tag that contains a kickstarter project
-        """
-        self.project_text = project_text
